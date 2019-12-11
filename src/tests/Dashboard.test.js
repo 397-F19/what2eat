ae88bef4082cb49eefb295f0cd915ade1d09af7e
weijianli <leevi1044@outlook.com>
2019-12-11 16:25:37 -0600

    add test for Dashboard.js

---
+++ src/tests/Dashboard.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render, fireEvent, waitForElement, findByRole } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import axiosMock from 'axios'
+import Dashboard from '../views/Dashboard/Dashboard.js';
+import { tsExternalModuleReference } from '@babel/types';
+import { get } from 'http';
+
+test('creat a new event', async () => {
+    const jsdomAlert = window.alert;
+    window.alert = () => {};
+
+
+    const creatingEventTest = jest.fn();
+
+    const { getByText } = render(
+    <Dashboard 
+    creatingEventTest={creatingEventTest}
+    />)
+
+    expect(getByText('Create New Event')).toBeInTheDocument()
+
+    fireEvent.click(getByText('Create New Event'))
+    expect(creatingEventTest).toHaveBeenCalledTimes(1)
+})
